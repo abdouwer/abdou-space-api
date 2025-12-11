13a7478b7a3cd2ae8d75d857604df39909e79d85
abdouramane <abdouwer93@gmail.com>
2025-12-12 00:10:31 +0100

    Change the title size and color

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 const Title = ({ text }: { text: string }): ReactNode => {
 	return (
 		<div>
-			<h3 className="my-6 text-4xl capitalize text-blue-900 p-2"><i className="fa-regular fa-newspaper"></i>  {text}</h3>
+			<h2 className="my-6 text-4xl capitalize text-yellow-700 p-2"><i className="fa-regular fa-newspaper"></i>  {text}</h2>
 		</div>
 	);
 };
